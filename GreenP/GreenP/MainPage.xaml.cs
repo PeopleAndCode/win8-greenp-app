d411c6c000aba793ac39df049dd8d6d7406ecbfb
Dan Zapornikov <danzap95@gmail.com>
2013-05-30 18:12:34 -0400

    Working commit #1
    Everything works, including the list and information display. Because
    bing maps does not work without internet, the program will exit without
    internet access. Potential improvements are possible.

--- GreenP/GreenP/MainPage.xaml.cs
+++ GreenP/GreenP/MainPage.xaml.cs
@@ -27,25 +27,102 @@ namespace GreenP
     {
 
         Dictionary<Pushpin, JsonObject> data;
-        
+        Dictionary<ListBoxItem, Pushpin> listData;
+
         public MainPage()
         {
             this.InitializeComponent();
 
+            try{
             data = Util.doInit();
 
             getPosition();
 
+            listData = new Dictionary<ListBoxItem, Pushpin>();
+
             foreach (Pushpin point in data.Keys)
             {
                 point.Text = "P";
+                ListBoxItem item = new ListBoxItem();
+                item.Tapped += new TappedEventHandler(listTapped);
+                item.Content = data[point]["address"].GetString();
+                listData[item] = point;
+                listBox.Items.Add(item);
+                point.Tapped += new TappedEventHandler(pinEvent);
                 double lat = 0;
                 double lng = 0;
-                Double.TryParse(data[point]["lat"].GetString(),out lat);
-                Double.TryParse(data[point]["lng"].GetString(),out lng);
-                MapLayer.SetPosition(point, new Location (lat,lng));
+                Double.TryParse(data[point]["lat"].GetString(), out lat);
+                Double.TryParse(data[point]["lng"].GetString(), out lng);
+                MapLayer.SetPosition(point, new Location(lat, lng));
                 myMap.Children.Add(point);
             }
+            }
+            catch
+            {
+                failGracefully();
+            }
+        }
+
+        private async void failGracefully()
+        {
+            MessageDialog m = new MessageDialog("This app can not function without internet access. Please connect to the internet and try again.");
+            await m.ShowAsync();
+            Environment.FailFast("Could not connect to the internet!");
+        }
+
+        private void pinEvent(object sender, TappedRoutedEventArgs e)
+        {
+            Pushpin p = sender as Pushpin;
+            myMap.SetView(MapLayer.GetPosition(p), 14);
+            myMap.SetView(MapLayer.GetPosition(p), 16);
+            infoBlock.Text = infoString(p);
+            rateBlock.Text = rateString(p);
+        }
+
+        private string rateString(Pushpin p)
+        {
+            string ret = "";
+
+            foreach (JsonValue val in data[p]["rate_details"].GetObject()["periods"].GetArray())
+            {
+                ret += val.GetObject()["title"].GetString() + "\n";
+                foreach (JsonValue v in val.GetObject()["rates"].GetArray())
+                {
+                    ret += "   - " + v.GetObject()["when"].GetString() + " : " + v.GetObject()["rate"].GetString() + "\n";
+                }
+            }
+
+            ret += "Payment Options :\n";
+            foreach (JsonValue val in data[p]["payment_options"].GetArray())
+            {
+                ret += "   - " + val.GetString() + "\n";
+            }
+
+            ret += "You can pay with :\n";
+            foreach (JsonValue val in data[p]["payment_methods"].GetArray())
+            {
+                ret +=  "   - " + val.GetString() + "\n";
+            }
+            return ret;
+        }
+
+        private string infoString(Pushpin p)
+        {
+            string ret = "";
+
+            ret += data[p]["address"].GetString() + "\n";
+            try { ret += data[p]["rate"].GetString() + "\n"; }
+            catch { ret += "See rate details on next slide \n"; }
+            ret += "Carpark Type : " + data[p]["carpark_type_str"].GetString() + "\n";
+            try
+            {
+                if (data[p]["max_height"].GetString() != "0.00")
+                    ret += "Maximum height : " + data[p]["max_height"].GetString() + " Meters\n";
+            }
+            catch { }
+            ret += "Maximum Capacity : " + data[p]["capacity"].GetString() + "\n";
+
+            return ret;
         }
 
         private async void getPosition()
@@ -54,7 +131,7 @@ namespace GreenP
             Geolocator geo = new Geolocator();
             Geoposition position = await geo.GetGeopositionAsync();
             Location loc = new Location(position.Coordinate.Latitude, position.Coordinate.Longitude);
-            myMap.SetView(loc, 12);
+            myMap.SetView(loc, 13);
         }
 
         /// <summary>
@@ -65,5 +142,16 @@ namespace GreenP
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+
+        private void FlipView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void listTapped(object sender, TappedRoutedEventArgs e)
+        {
+            pinEvent(listData[sender as ListBoxItem], e);
+        }
     }
+
 }
